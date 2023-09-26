2d436d76162cd16f40e919f5677b76f7a86ccfa8
sharov_vitalii <fly44088@gmail.com>
2023-09-27 02:12:39 +0300

    Выполненное ДЗ к 4 уроку

--- test_homework.py
+++ test_homework.py
@@ -82,6 +82,7 @@ def test_dicts():
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
     d = dict(zip(first, second))
+    print(d.values())
 
     assert isinstance(d, dict)
     assert len(d) == 5
